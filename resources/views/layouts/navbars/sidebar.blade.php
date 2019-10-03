df4080e7d080430dd0a03ac0fd21914e29886980
vinicosta <vinicosta@gmail.com>
2019-10-03 10:57:15 -0300

    Arquitetura básica do Laravel + Material Dashboard

---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,85 @@
+<div class="sidebar">{{-- data-color="orange" data-background-color="base02"
+data-image="{{ asset('material') }}/img/sidebar-1.jpg" --}}
+    <!--
+      Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
+
+      Tip 2: you can also add an image using data-image tag
+  -->
+    <div class="logo">
+        <a href="https://creative-tim.com/" class="simple-text logo-normal">
+            <i><img style="width:25px; border: 1px solid #93a1a1" src="{{ asset('material') }}/img/saladejustica.png"></i>
+            {{ __(config('app.name')) }}
+        </a>
+    </div>
+    <div class="sidebar-wrapper">
+        <ul class="nav">
+            <li class="nav-item{{ $activePage == 'dashboard' ? ' active' : '' }}">
+                <a class="nav-link" href="{{ route('home') }}">
+                    <i class="material-icons">dashboard</i>
+                    <p>{{ __('Dashboard') }}</p>
+                </a>
+            </li>
+            <li class="nav-item {{ ($activePage == 'profile' || $activePage == 'user-management') ? ' active' : '' }}">
+                <a class="nav-link" data-toggle="collapse" href="#cadastros" aria-expanded="true">
+                    <i class="material-icons">table_chart</i>
+                    <p>{{ __('Cadastros') }}
+                        <b class="caret"></b>
+                    </p>
+                </a>
+                <div class="collapse show" id="cadastros">
+                    <ul class="nav">
+                        <li class="nav-item{{ $activePage == 'user-management' ? ' active' : '' }}">
+                            <a class="nav-link" href="{{ route('user.index') }}">
+                                <i class="material-icons">person</i>
+                                <span class="sidebar-normal"> {{ __('Usuários') }} </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            {{--
+            <li class="nav-item{{ $activePage == 'table' ? ' active' : '' }}">
+                <a class="nav-link" href="{{ route('table') }}">
+                    <i class="material-icons">content_paste</i>
+                    <p>{{ __('Table List') }}</p>
+                </a>
+            </li>
+            <li class="nav-item{{ $activePage == 'typography' ? ' active' : '' }}">
+                <a class="nav-link" href="{{ route('typography') }}">
+                    <i class="material-icons">library_books</i>
+                    <p>{{ __('Typography') }}</p>
+                </a>
+            </li>
+            <li class="nav-item{{ $activePage == 'icons' ? ' active' : '' }}">
+                <a class="nav-link" href="{{ route('icons') }}">
+                    <i class="material-icons">bubble_chart</i>
+                    <p>{{ __('Icons') }}</p>
+                </a>
+            </li>
+            <li class="nav-item{{ $activePage == 'map' ? ' active' : '' }}">
+                <a class="nav-link" href="{{ route('map') }}">
+                    <i class="material-icons">location_ons</i>
+                    <p>{{ __('Maps') }}</p>
+                </a>
+            </li>
+            <li class="nav-item{{ $activePage == 'notifications' ? ' active' : '' }}">
+                <a class="nav-link" href="{{ route('notifications') }}">
+                    <i class="material-icons">notifications</i>
+                    <p>{{ __('Notifications') }}</p>
+                </a>
+            </li>
+            <li class="nav-item{{ $activePage == 'language' ? ' active' : '' }}">
+                <a class="nav-link" href="{{ route('language') }}">
+                    <i class="material-icons">language</i>
+                    <p>{{ __('RTL Support') }}</p>
+                </a>
+            </li>
+            <li class="nav-item active-pro{{ $activePage == 'upgrade' ? ' active' : '' }}">
+                <a class="nav-link" href="{{ route('upgrade') }}">
+                    <i class="material-icons">unarchive</i>
+                    <p>{{ __('Upgrade to PRO') }}</p>
+                </a>
+            </li> --}}
+        </ul>
+    </div>
+</div>
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.app', ['activePage' => 'user-management', 'titlePage' => __('Gestāo de Usuários')])
+
+@section('content')
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <form method="post" action="{{ route('user.store') }}" autocomplete="off" class="form-horizontal">
+                    @csrf
+                    @method('post')
+
+                    <div class="card ">
+                        <div class="card-header card-header-primary">
+                            <h4 class="card-title">{{ __('Adicionar Usuário') }}</h4>
+                            <p class="card-category"></p>
+                        </div>
+                        <div class="card-body ">
+                            <div class="row">
+                                <div class="col-md-12 text-right">
+                                    <a href="{{ route('user.index') }}"
+                                        class="btn btn-sm btn-primary">{{ __('Voltar') }}</a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-sm-2 col-form-label">{{ __('Nome') }}</label>
+                                <div class="col-sm-7">
+                                    <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
+                                        <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                                            name="name" id="input-name" type="text" placeholder="{{ __('Nome') }}"
+                                            value="{{ old('name') }}" required="true" aria-required="true" />
+                                        @if ($errors->has('name'))
+                                        <span id="name-error" class="error text-danger"
+                                            for="input-name">{{ $errors->first('name') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-sm-2 col-form-label">{{ __('E-mail') }}</label>
+                                <div class="col-sm-7">
+                                    <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                                        <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                            name="email" id="input-email" type="email" placeholder="{{ __('E-mail') }}"
+                                            value="{{ old('email') }}" required />
+                                        @if ($errors->has('email'))
+                                        <span id="email-error" class="error text-danger"
+                                            for="input-email">{{ $errors->first('email') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-sm-2 col-form-label"
+                                    for="input-password">{{ __(' Senha') }}</label>
+                                <div class="col-sm-7">
+                                    <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                                        <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                            input type="password" name="password" id="input-password"
+                                            placeholder="{{ __('Senha') }}" value="" required />
+                                        @if ($errors->has('password'))
+                                        <span id="name-error" class="error text-danger"
+                                            for="input-name">{{ $errors->first('password') }}</span>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-sm-2 col-form-label"
+                                    for="input-password-confirmation">{{ __('Confirme a senha') }}</label>
+                                <div class="col-sm-7">
+                                    <div class="form-group">
+                                        <input class="form-control" name="password_confirmation"
+                                            id="input-password-confirmation" type="password"
+                                            placeholder="{{ __('Confirme a senha') }}" value="" required />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer ml-auto mr-auto">
+                            <button type="submit" class="btn btn-primary">{{ __('Adicionar') }}</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
